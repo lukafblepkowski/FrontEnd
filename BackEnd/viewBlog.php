58bf048eeae99fdbab8cd158f54b9f9456bbc191
lukafblepkowski <lukalepkowski@gmail.com>
2025-04-15 19:35:13 +0100

    Blog is displayed now

--- BackEnd/viewBlog.php
+++ BackEnd/viewBlog.php
@@ -1,13 +1,47 @@
 <?php
-            $given_email = $_POST['email'];
-            $given_password = $_POST['password'];
+    session_start();
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "website";
+
+    #Connect
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    #Check connection is okay
+    if($conn->connect_error) {
+        exit("Connection failed: " . $conn->connect_error);
+    }  
     
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "website";
-    
-            $conn = new mysqli($servername, $username, $password, $dbname);
+    #Retrieve posts
+    $sql = $conn->prepare("SELECT * FROM posts");
+    $sql->execute();
+    $result = $sql->get_result();
+
+    $rows = [];
+    if($result->num_rows > 0) {
+        //generate array
+        while($row = $result->fetch_assoc()) {
+            $_sql = $conn->prepare("SELECT email FROM users WHERE user_id = ?");
+            $_sql->bind_param('i', $row['author_id']);
+            $_sql->execute();
+            $_result = $_sql->get_result();
+
+            if($_result->num_rows == 1) {
+                $r = $_result->fetch_assoc();
+                $row['author'] = $r['email'];
+            }
+
+            array_push($rows, $row);
+        }
+    }
+
+    #If there's no blog, go home !
+    if(count($rows) == 0) {
+        header("Location: ");
+        exit();
+    }
 ?>
 
 <html>
@@ -33,16 +67,43 @@
             </div>
 
             <div class="blogposts">
-                <article class="blogpost">
-                    <div class="blogpost-header">
-                        <div class="blogpost-title"></div>
-                        <div class="blogpost-date">(Written)</div>
-                    </div>
-                    
-                    <div class="blogpost-content">
+                <?php 
+                    for($i = 0; $i < count($rows); $i++) {
+                        $row = $rows[$i];
+                ?>
+                    <article class="blogpost">
+                        <div class="blogpost-header">
+                            <div class="blogpost-title">
+                                <?php  
+                                    echo htmlspecialchars($row['title'])
+                                ?>
+                            </div>
 
-                    </div>
-                </article>
+                            <div class="blogpost-date">
+                                (Written
+                                <?php 
+                                    $time = strtotime($row['timestamp']);
+                                    echo date("F j Y", $time);
+                                ?>)
+                            </div>
+                        </div>
+                        
+                        <div class="blogpost-content">
+                            <?php
+                                #NL2BR converts new lines into <br> tag 
+                                echo nl2br(htmlspecialchars($row['content']))
+                            ?>
+                        </div>
+
+                        <div class="blogpost-author">
+                            By <?php
+                                echo htmlspecialchars($row['author']);
+                            ?>
+                        </div>
+                    </article>
+                <?php
+                    }
+                ?>
             </div>
         </section>
     </body>
